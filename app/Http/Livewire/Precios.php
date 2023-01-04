8afa3968e02ac3755caa1c4d8148987d8a3a2cee
Kenet Picado <kenetpicado1@gmail.com>
2023-01-04 16:36:09 -0600

    precios

--- app/Http/Livewire/Precios.php
+++ app/Http/Livewire/Precios.php
@@ -4,48 +4,54 @@ namespace App\Http\Livewire;
 
 use App\Models\Precio;
 use App\Traits\MyAlerts;
-use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Precios extends Component
 {
     use MyAlerts;
-    
-    public $sub_id, $servicio, $plan, $monto;
+
+    public $precio = null;
+
+    public function mount()
+    {
+        $this->precio = new Precio();
+    }
+
+    protected $rules = [
+        'precio.monto'      => 'required|numeric',
+        'precio.servicio'   => 'required',
+        'precio.plan'       => 'required'
+    ];
+
+    public function render()
+    {
+        return view('livewire.precios', [
+            'precios' => Precio::all()
+        ]);
+    }
+
+    public function edit(Precio $precio)
+    {
+        $this->precio = $precio;
+        $this->emit('open-create-modal');
+    }
+
+    public function store()
+    {
+        $data = $this->validate();
+        $this->precio->save($data);
+
+        $this->success(true);
+        $this->resetInputFields();
+
+        $this->emit('close-create-modal');
+    }
 
     public function resetInputFields()
     {
         $this->reset();
         $this->resetErrorBag();
-    }
 
-    public function render()
-    {
-        $precios = DB::table('precios')->get();
-        return view('livewire.precios', compact('precios'));
-    }
-
-    /* Cargar modal para editar un precio */
-    public function edit($precio_id)
-    {
-        $precio = DB::table('precios')->find($precio_id);
-        $this->sub_id = $precio->id;
-        $this->servicio = $precio->servicio;
-        $this->plan = $precio->plan;
-        $this->monto = $precio->monto;
-        $this->emit('open-create-modal');
-    }
-
-     /* Guardar precio */
-    public function store()
-    {
-        $data = $this->validate([
-            'monto' => 'required|numeric|gt:0'
-        ]);
-
-        Precio::updateOrCreate(['id' => $this->sub_id], $data);
-        $this->success(1);
-        $this->resetInputFields();
-        $this->emit('close-create-modal');
+        $this->precio = new Precio();
     }
 }
